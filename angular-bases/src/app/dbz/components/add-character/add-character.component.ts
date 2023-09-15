d107dc635f4fa7d5366004e5b6d3dd4ea3a2d5d9
Bryan Valerin <bryanvalerin@gmail.com>
2023-09-15 12:05:35 -0600

    Primer commit

---
+++ angular-bases/src/app/dbz/components/add-character/add-character.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-dbz-add-character',
+  templateUrl: './add-character.component.html',
+  styleUrls: ['./add-character.component.css']
+})
+
+
+export class AddCharacterComponent {
+
+}
